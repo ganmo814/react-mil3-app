2e5581ee46a730f99f342276afa453e12d81051d
ganmo814 <ganmo814@gmail.com>
2022-05-10 12:12:09 +0900

    404ページ修正

--- src/components/pages/Page404.tsx
+++ src/components/pages/Page404.tsx
@@ -3,6 +3,6 @@ import { memo, VFC } from "react";
 
 export const Page404: VFC = memo(() => {
     return (
-        <Heading mt={5} ml={5} color="gray.400">404ページです</Heading>
+        <Heading mt={5} ml={5} color="gray.400"></Heading>
     );
 });
